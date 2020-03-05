86c2c1f71736609fd87fc35d51871244587f808e
ctb1321 <31220600+ctb1321@users.noreply.github.com>
2020-03-04 21:52:10 -0600

    Initial SSL implementation

--- Robot.py
+++ Robot.py
@@ -1,44 +1,72 @@
 import random
 import socket
+import ssl
+import sys
 import time
 
 #list of characters to use for building random string
 char_set =  ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-#create first socket and listen for a connection from client
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_1:
-	s_1.bind(("127.0.0.1", 3310))
-	print("ROBOT IS STARTED")
-	s_1.listen()
-	connection, address = s_1.accept()
-	
-	#when client connects, print BlazerID then send a port number to client
-	with connection:
-		print(str(connection.recv(1024), "utf-8"))
-		connection.send((12345).to_bytes(10, byteorder="big"))
+try:
+	if sys.argv[1] == "-s":
 
-#wait 1 second, create second socket connection to client at the port number we sent
-time.sleep(1.0)
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_2:
-	s_2.connect(("127.0.0.1", 12345))
-	s_2.send(bytes("23456,65432.", "utf-8"))
+		#context for SSL
+		context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 
-#create third socket, bind, then connect
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_3:
-	s_3.bind(("127.0.0.1", 23456))
-	s_3.connect(("127.0.0.1", 65432))
-	
-	#translate random number received from client into a string
-	random_int = int.from_bytes(s_3.recv(1024), byteorder="big")*10
-	random_char_string = str(random.choices(char_set, k=random_int))
-	
-	#for 5 iterations, wait 1 second, then send char string to client
-	for i in range(5):
+		#create first SSL socket and listen for a connection from client
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_1:
+			s_1.bind(("127.0.0.1", 27994))
+			print("ROBOT IS STARTED with SSL")
+			s_1.listen()
+			
+			#wrap first socket with an SSL context
+			with context.wrap_socket(s_1, server_side=True) as secure_s_1:
+				connection, address = secure_s_1.accept()
+				
+				#when client connects, print BlazerID then send a port number to client
+				with connection:
+					print(str(connection.recv(1024), "utf-8"))
+					connection.send((12345).to_bytes(10, byteorder="big"))
+
+	else:
+		print("Invalid argument, please use '-s' to connect via SSL")
+
+except IndexError:
+
+	#create first socket and listen for a connection from client
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_1:
+			s_1.bind(("127.0.0.1", 3310))
+			print("ROBOT IS STARTED")
+			s_1.listen()
+			connection, address = s_1.accept()
+			
+			#when client connects, print BlazerID then send a port number to client
+			with connection:
+				print(str(connection.recv(1024), "utf-8"))
+				connection.send((12345).to_bytes(10, byteorder="big"))
+
+		#wait 1 second, create second socket connection to client at the port number we sent
 		time.sleep(1.0)
-		s_3.send(bytes(random_char_string, "utf-8"))
-		client_response = str(s_3.recv(1024), "utf-8")
-		#if the client responds with a matching string, break from loop
-		if (random_char_string == client_response):
-			s_3.send(bytes("success", "utf-8"))
-			print("success")
-			break
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_2:
+			s_2.connect(("127.0.0.1", 12345))
+			s_2.send(bytes("23456,65432.", "utf-8"))
+
+		#create third socket, bind, then connect
+		with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_3:
+			s_3.bind(("127.0.0.1", 23456))
+			s_3.connect(("127.0.0.1", 65432))
+			
+			#translate random number received from client into a string
+			random_int = int.from_bytes(s_3.recv(1024), byteorder="big")*10
+			random_char_string = str(random.choices(char_set, k=random_int))
+			
+			#for 5 iterations, wait 1 second, then send char string to client
+			for i in range(5):
+				time.sleep(1.0)
+				s_3.send(bytes(random_char_string, "utf-8"))
+				client_response = str(s_3.recv(1024), "utf-8")
+				#if the client responds with a matching string, break from loop
+				if (random_char_string == client_response):
+					s_3.send(bytes("success", "utf-8"))
+					print("success")
+					break
--- Student.py
+++ Student.py
@@ -1,12 +1,51 @@
 import random
 import socket
-import time
+import ssl
 import sys
+import time
 
 #check arguments passed from the command line
 try:
 	if sys.argv[1] == "-s":
-		print("SSL connection")
+		#context and hostname for SSL
+		context = ssl.create_default_context()
+		hostname = "127.0.0.1"
+		
+		#create first socket and wrap in SSL context
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_1:
+			with context.wrap_socket(s_1, server_hostname=hostname) as secure_s_1:
+				#connect and send BlazerID to server
+				secure_s_1.connect(("127.0.0.1", 27994))
+				secure_s_1.send(bytes("ctbice66", "utf-8"))
+				
+				#create second socket, bind and listen for connection from server
+				with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_2:
+					s_2.bind(("127.0.0.1", int.from_bytes(secure_s_1.recv(1024), byteorder="big")))
+					s_2.listen()
+					connection, address = s_2.accept()
+					
+					#when server connects, extract ports
+					with connection:
+						ports = str(connection.recv(1024), "utf-8").split(",")
+						server_port = int(ports[0])
+						client_port = int(ports[1].split(".")[0])
+						
+						#create third socket - UDP, bind and then connect to server
+						with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_3:
+							s_3.bind(("127.0.0.1", client_port))
+							s_3.connect(("127.0.0.1", server_port))
+							#send random int to server
+							s_3.send(bytes(random.randint(6, 9).to_bytes(10, byteorder="big")))
+							
+							#send response back to server five times; once per second
+							for i in range(5):
+								time.sleep(1.0)
+								s_3.send(s_3.recv(1024))
+								
+								#if server responds with success message, break from loop
+								if (str(s_3.recv(1024), "utf-8") == "success"):
+									print("success")
+									break
 	else:
 		print("Invalid argument, please use '-s' to connect via SSL")
 
