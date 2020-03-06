d6616aa2ed1a1eb0ba5bb786f349d2936c699358
ctb1321 <31220600+ctb1321@users.noreply.github.com>
2020-03-06 14:08:44 -0600

    Cleanup; prints cert to console

--- Robot.py
+++ Robot.py
@@ -7,21 +7,24 @@ import time
 #list of characters to use for building random string
 char_set =  ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-#return a random port greater than 1024 but less than or equal to 65535; exclude 3310 and 27994
-def randomPort():
+#list of ports in use
+usedPorts = [3310, 27994]
+
+#return a random port greater than 1024 but less than or equal to 65535; exclude 3310, 27994, and any already selected for use
+def getRandomPort():
 	port = random.randint(1025, 65535)
 	
-	if (port == 3310):
-		randomPort()
-	elif (port == 27994):
+	#check if port is already in use, if not then return port and add it to list of ports in use
+	if (port in usedPorts):
 		randomPort()
 	else:
+		usedPorts.append(port)
 		return port
 
 #select 3 random ports for implementation
-s_2_port = randomPort()
-server_port = randomPort()
-client_port = randomPort()
+s_2_port = getRandomPort()
+server_port = getRandomPort()
+client_port = getRandomPort()
 
 #check for arguments passed to server
 if (len(sys.argv) > 1):
@@ -32,140 +35,138 @@ if (len(sys.argv) > 1):
 		context.load_cert_chain('cert.pem')
 		
 		#create first SSL socket and listen for a connection from client; set 5 second timeout on socket
-		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_1:
-			s_1.settimeout(5.0)
-			s_1.bind(("127.0.0.1", 27994))
-			print("ROBOT IS STARTED with SSL")
-			s_1.listen()
-			print("Listening on port 27994")
-			
-			#wrap first socket with an SSL context
-			with context.wrap_socket(s_1, server_side=True) as secure_s_1:
-				connection, address = secure_s_1.accept()
-				
-				#when client connects, print BlazerID then send a port number to client
-				with connection:
-					print("Connection to port 27994 successful")
-					print(str(connection.recv(1024), "utf-8"))
-					connection.send((s_2_port).to_bytes(10, byteorder="big"))
-					
-					#close first socket
-					secure_s_1.close()
-			
-			#wait 1 second, create second socket connection to client at the port number we sent
+		s_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s_1.settimeout(5.0)
+		s_1.bind(("127.0.0.1", 27994))
+		print("ROBOT IS STARTED with SSL")
+		s_1.listen()
+		print("Listening on port 27994")
+		
+		#wrap first socket with an SSL context
+		secure_s_1 = context.wrap_socket(s_1, server_side=True)
+		connection, address = secure_s_1.accept()
+		
+		#when client connects, print BlazerID then send a port number to client
+		print("Connection to port 27994 successful")
+		print(str(connection.recv(1024), "utf-8"))
+		connection.send((s_2_port).to_bytes(10, byteorder="big"))
+		
+		#close first socket
+		secure_s_1.close()
+		
+		#wait 1 second, create second socket connection to client at the port number we sent
+		time.sleep(1.0)
+		s_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s_2.connect(("127.0.0.1", s_2_port))
+		print("Connected to client at port {}".format(s_2_port))
+		
+		#send ports selected at random to client via second socket
+		s_2.send(bytes(str(server_port) + "," + str(client_port) + ".", "utf-8"))
+		print("Sending server port {} and client port {}".format(server_port, client_port))
+		
+		#close second socket
+		s_2.close()
+		
+		#create third socket - UDP, bind to server port, set 5 second timeout
+		s_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s_3.bind(("127.0.0.1", server_port))
+		s_3.settimeout(5.0)
+		print("Waiting for random int from client on port {}".format(server_port))
+		
+		#receive random int value from client, multiply by 10 to generate size of random character string
+		random_int = int.from_bytes(s_3.recv(1024), byteorder="big")
+		random_string_size = random_int * 10
+		print("Received random int from client on port {}".format(server_port))
+		
+		#translate random number received from client into a character string
+		random_char_string = "".join(random.choices(char_set, k=random_string_size))
+		print("Random character string selected: {}".format(random_char_string))
+		
+		#connect to client via UDP
+		s_3.connect(("127.0.0.1", client_port))
+		print("Connected to client at port {}".format(client_port))
+		
+		#for 5 iterations, wait 1 second, then send random character string to client
+		for i in range(5):
 			time.sleep(1.0)
-			with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_2:
-				s_2.connect(("127.0.0.1", s_2_port))
-				print("Connected to client at port {}".format(s_2_port))
+			s_3.send(bytes(random_char_string, "utf-8"))
+			print("Sending random character string to client on port {}".format(client_port))
+			
+			#if the client responds with a matching string, break from loop
+			if (random_char_string == str(s_3.recv(1024), "utf-8")):
+				s_3.send(bytes("success", "utf-8"))
+				print("Client has responded with a matching string")
 				
-				#send ports selected at random to client via second socket
-				s_2.send(bytes(str(server_port) + "," + str(client_port) + ".", "utf-8"))
-				print("Sending server port {} and client port {}".format(server_port, client_port))
+				#close third socket
+				s_3.close()
 				
-				#close second socket
-				s_2.close()
-				
-				#create third socket - UDP, bind to server port, set 5 second timeout
-				s_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-				s_3.bind(("127.0.0.1", server_port))
-				s_3.settimeout(5.0)
-				print("Waiting for random int from client on port {}".format(server_port))
-				
-				#receive random int value from client, multiply by 10 to generate size of random character string
-				random_int = int.from_bytes(s_3.recv(1024), byteorder="big")
-				random_string_size = random_int * 10
-				print("Received random int from client on port {}".format(server_port))
-				
-				#translate random number received from client into a character string
-				random_char_string = str(random.choices(char_set, k=random_string_size))
-				print("Random character string selected: {}".format(random_char_string))
-				
-				#connect to client via UDP
-				s_3.connect(("127.0.0.1", client_port))
-				print("Connected to client at port {}".format(client_port))
-				
-				#for 5 iterations, wait 1 second, then send random character string to client
-				for i in range(5):
-					time.sleep(1.0)
-					s_3.send(bytes(random_char_string, "utf-8"))
-					print("Sending random character string to client on port {}".format(client_port))
-					
-					#if the client responds with a matching string, break from loop
-					if (random_char_string == str(s_3.recv(1024), "utf-8")):
-						s_3.send(bytes("success", "utf-8"))
-						print("Client has responded with a matching string")
-						
-						#close third socket
-						s_3.close()
-						
-						break
+				break
 	else:
 		print("Invalid argument, please use '-s' to connect via SSL")
 
 else:
 
-		#create first socket and listen for a connection from client; set 5 second timeout on socket
-		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_1:
-			s_1.settimeout(5.0)
-			s_1.bind(("127.0.0.1", 3310))
-			print("ROBOT IS STARTED")
-			s_1.listen()
-			print("Listening on port 3310")
-			connection, address = s_1.accept()
-			
-			#when client connects, print BlazerID then send a port number to client
-			with connection:
-				print("Connection to port 3310 successful")
-				print(str(connection.recv(1024), "utf-8"))
-				connection.send((s_2_port).to_bytes(10, byteorder="big"))
-				
-				#close first socket
-				s_1.close()
-		
-		#wait 1 second, create second socket connection to client at the port number we sent
+	#create first socket and listen for a connection from client; set 5 second timeout on socket
+	s_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s_1.settimeout(5.0)
+	s_1.bind(("127.0.0.1", 3310))
+	print("ROBOT IS STARTED")
+	s_1.listen()
+	print("Listening on port 3310")
+	connection, address = s_1.accept()
+	
+	#when client connects, print BlazerID then send a port number to client
+	print("Connection to port 3310 successful")
+	print(str(connection.recv(1024), "utf-8"))
+	connection.send((s_2_port).to_bytes(10, byteorder="big"))
+	
+	#close first socket
+	s_1.close()
+	
+	#wait 1 second, create second socket connection to client at the port number we sent
+	time.sleep(1.0)
+	s_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s_2.connect(("127.0.0.1", s_2_port))
+	print("Connected to client at port {}".format(s_2_port))
+	
+	#send ports selected at random to client via second socket
+	s_2.send(bytes(str(server_port) + "," + str(client_port) + ".", "utf-8"))
+	print("Sending server port {} and client port {}".format(server_port, client_port))
+	
+	#close second socket
+	s_2.close()
+	
+	#create third socket - UDP, bind to server port, set 5 second timeout
+	s_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s_3.bind(("127.0.0.1", server_port))
+	s_3.settimeout(5.0)
+	print("Waiting for random int from client on port {}".format(server_port))
+	
+	#receive random int value from client, multiply by 10 to generate size of random character string
+	random_int = int.from_bytes(s_3.recv(1024), byteorder="big")
+	random_string_size = random_int * 10
+	print("Received random int from client on port {}".format(server_port))
+	
+	#translate random number received from client into a character string
+	random_char_string = "".join(random.choices(char_set, k=random_string_size))
+	print("Random character string selected: {}".format(random_char_string))
+	
+	#connect to client via UDP
+	s_3.connect(("127.0.0.1", client_port))
+	print("Connected to client at port {}".format(client_port))
+	
+	#for 5 iterations, wait 1 second, then send random character string to client
+	for i in range(5):
 		time.sleep(1.0)
-		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_2:
-			s_2.connect(("127.0.0.1", s_2_port))
-			print("Connected to client at port {}".format(s_2_port))
+		s_3.send(bytes(random_char_string, "utf-8"))
+		print("Sending random character string to client on port {}".format(client_port))
+		
+		#if the client responds with a matching string, break from loop
+		if (random_char_string == str(s_3.recv(1024), "utf-8")):
+			s_3.send(bytes("success", "utf-8"))
+			print("Client has responded with a matching string")
 			
-			#send ports selected at random to client via second socket
-			s_2.send(bytes(str(server_port) + "," + str(client_port) + ".", "utf-8"))
-			print("Sending server port {} and client port {}".format(server_port, client_port))
+			#close third socket
+			s_3.close()
 			
-			#close second socket
-			s_2.close()
-			
-			#create third socket - UDP, bind to server port, set 5 second timeout
-			s_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			s_3.bind(("127.0.0.1", server_port))
-			s_3.settimeout(5.0)
-			print("Waiting for random int from client on port {}".format(server_port))
-			
-			#receive random int value from client, multiply by 10 to generate size of random character string
-			random_int = int.from_bytes(s_3.recv(1024), byteorder="big")
-			random_string_size = random_int * 10
-			print("Received random int from client on port {}".format(server_port))
-			
-			#translate random number received from client into a character string
-			random_char_string = str(random.choices(char_set, k=random_string_size))
-			print("Random character string selected: {}".format(random_char_string))
-			
-			#connect to client via UDP
-			s_3.connect(("127.0.0.1", client_port))
-			print("Connected to client at port {}".format(client_port))
-			
-			#for 5 iterations, wait 1 second, then send random character string to client
-			for i in range(5):
-				time.sleep(1.0)
-				s_3.send(bytes(random_char_string, "utf-8"))
-				print("Sending random character string to client on port {}".format(client_port))
-				
-				#if the client responds with a matching string, break from loop
-				if (random_char_string == str(s_3.recv(1024), "utf-8")):
-					s_3.send(bytes("success", "utf-8"))
-					print("Client has responded with a matching string")
-					
-					#close third socket
-					s_3.close()
-					
-					break
+			break
--- Student.py
+++ Student.py
@@ -14,76 +14,75 @@ if (len(sys.argv) > 1):
 		context.check_hostname = False
 		
 		#create first socket and wrap in SSL context
-		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_1:
-			with context.wrap_socket(s_1, server_hostname="127.0.0.1") as secure_s_1:
+		s_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		secure_s_1 = context.wrap_socket(s_1, server_hostname="127.0.0.1")
+		
+		#connect and send BlazerID to server
+		secure_s_1.connect(("127.0.0.1", 27994))
+		print("Verified server using certificate: {}".format(secure_s_1.getpeercert()))
+		print("Connected to server on port 27994")
+		secure_s_1.send(bytes("ctbice66", "utf-8"))
+		print("Sending BlazerID to server on port 27994")
+		
+		#create second socket, bind and listen for connection from server; set 5 second timeout on socket
+		s_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s_2.settimeout(5.0)
+		
+		#remove comment to work with instructor-provided ROBOT executable
+		#s_2_port = int(s_1.recv(1024).decode("utf-8"))
+		
+		s_2_port = int.from_bytes(secure_s_1.recv(1024), byteorder="big")
+		s_2.bind(("127.0.0.1", s_2_port))
+		s_2.listen()
+		print("Listening on port {}".format(s_2_port))
+		connection, address = s_2.accept()
+		
+		#close first socket
+		secure_s_1.close()
+		
+		#remove comment to work with instructor-provided ROBOT executable
+		#ports = connection.recv(1024).decode("utf-8").split(",")
+		#server_port = int(ports[0])
+		#client_port = int(ports[1])
+		
+		#when server connects, extract ports
+		ports = str(connection.recv(1024), "utf-8").split(",")
+		server_port = int(ports[0])
+		client_port = int(ports[1].split(".")[0])
+		
+		#close second socket
+		s_2.close()
+		
+		#create third socket - UDP
+		s_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		
+		#bind UDP socket to client port, set 5 second timeout
+		s_3.bind(("127.0.0.1", client_port))
+		s_3.settimeout(5.0)
+		
+		#connect to server via UDP
+		s_3.connect(("127.0.0.1", server_port))
+		print("Connected to server on port {}".format(server_port))
+		
+		#wait for 1 second, then send random int to server
+		time.sleep(1.0)
+		s_3.send(bytes(random.randint(6, 9).to_bytes(10, byteorder="big")))
+		print("Sent random integer to server on port {}".format(server_port))
+		
+		#send random characters received as a response back to server five times; once per second
+		for i in range(5):
+			time.sleep(1.0)
+			s_3.send(s_3.recv(1024))
+			print("Sending random string of characters back to server on {}".format(server_port))
+			
+			#if server responds with success message, break from loop
+			if (str(s_3.recv(1024), "utf-8") == "success"):
+				print("Server confirmed matching character string")
 				
-				#connect and send BlazerID to server
-				secure_s_1.connect(("127.0.0.1", 27994))
-				print("Connected to server on port 27994")
-				secure_s_1.send(bytes("ctbice66", "utf-8"))
-				print("Sending BlazerID to server on port 27994")
+				#close third socket
+				s_3.close()
 				
-				#create second socket, bind and listen for connection from server; set 5 second timeout on socket
-				with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_2:
-					s_2.settimeout(5.0)
-					
-					#remove comment to work with instructor-provided ROBOT executable
-					#s_2_port = int(s_1.recv(1024).decode("utf-8"))
-					
-					s_2_port = int.from_bytes(secure_s_1.recv(1024), byteorder="big")
-					s_2.bind(("127.0.0.1", s_2_port))
-					s_2.listen()
-					print("Listening on port {}".format(s_2_port))
-					connection, address = s_2.accept()
-					
-					#when server connects, extract ports
-					with connection:
-						
-						#close first socket
-						secure_s_1.close()
-						
-						#remove comment to work with instructor-provided ROBOT executable
-						#ports = connection.recv(1024).decode("utf-8").split(",")
-						#server_port = int(ports[0])
-						#client_port = int(ports[1])
-						
-						ports = str(connection.recv(1024), "utf-8").split(",")
-						server_port = int(ports[0])
-						client_port = int(ports[1].split(".")[0])
-						
-						#close second socket
-						s_2.close()
-						
-						#create third socket - UDP
-						s_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-						
-						#bind UDP socket to client port, set 5 second timeout
-						s_3.bind(("127.0.0.1", client_port))
-						s_3.settimeout(5.0)
-						
-						#connect to server via UDP
-						s_3.connect(("127.0.0.1", server_port))
-						print("Connected to server on port {}".format(server_port))
-						
-						#wait for 1 second, then send random int to server
-						time.sleep(1.0)
-						s_3.send(bytes(random.randint(6, 9).to_bytes(10, byteorder="big")))
-						print("Sent random integer to server on port {}".format(server_port))
-						
-						#send random characters received as a response back to server five times; once per second
-						for i in range(5):
-							time.sleep(1.0)
-							s_3.send(s_3.recv(1024))
-							print("Sending random string of characters back to server on {}".format(server_port))
-							
-							#if server responds with success message, break from loop
-							if (str(s_3.recv(1024), "utf-8") == "success"):
-								print("Server confirmed matching character string")
-								
-								#close third socket
-								s_3.close()
-								
-								break
+				break
 	else:
 		print("Invalid argument, please use '-s' to connect via SSL")
 
@@ -91,69 +90,69 @@ if (len(sys.argv) > 1):
 else:
 	
 	#create first socket, connect and send BlazerID to server
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_1:
-		s_1.connect(("127.0.0.1", 3310))
-		print("Connected to server on port 3310")
-		s_1.send(bytes("ctbice66", "utf-8"))
-		print("Sending BlazerID to server on port 3310")
+	s_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s_1.connect(("127.0.0.1", 3310))
+	print("Connected to server on port 3310")
+	s_1.send(bytes("ctbice66", "utf-8"))
+	print("Sending BlazerID to server on port 3310")
+	
+	#create second socket, bind and listen for connection from server; set 5 second timeout on socket
+	s_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s_2.settimeout(5.0)
+	
+	#remove comment to work with instructor-provided ROBOT executable
+	#s_2_port = int(s_1.recv(1024).decode("utf-8"))
+	
+	s_2_port = int.from_bytes(s_1.recv(1024), byteorder="big")
+	s_2.bind(("127.0.0.1", s_2_port))
+	s_2.listen()
+	print("Listening on port {}".format(s_2_port))
+	connection, address = s_2.accept()
+	
+	#when server connects, extract ports
+	#if connection:
+	
+	#close first socket
+	s_1.close()
+	#remove comment to work with instructor-provided ROBOT executable
+	#ports = connection.recv(1024).decode("utf-8").split(",")
+	#server_port = int(ports[0])
+	#client_port = int(ports[1])
+	
+	ports = str(connection.recv(1024), "utf-8").split(",")
+	server_port = int(ports[0])
+	client_port = int(ports[1].split(".")[0])
+	
+	#close second socket
+	s_2.close()
+	
+	#create third socket - UDP
+	s_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	
+	#bind UDP socket to client port, set 5 second timeout
+	s_3.bind(("127.0.0.1", client_port))
+	s_3.settimeout(5.0)
+	
+	#connect to server via UDP
+	s_3.connect(("127.0.0.1", server_port))
+	print("Connected to server on port {}".format(server_port))
+	
+	#wait for 1 second, then send random int to server
+	time.sleep(1.0)
+	s_3.send(bytes(random.randint(6, 9).to_bytes(10, byteorder="big")))
+	print("Sent random integer to server on port {}".format(server_port))
+	
+	#send random characters received as a response back to server five times; once per second
+	for i in range(5):
+		time.sleep(1.0)
+		s_3.send(s_3.recv(1024))
+		print("Sending random string of characters back to server on {}".format(server_port))
 		
-		#create second socket, bind and listen for connection from server; set 5 second timeout on socket
-		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_2:
-			s_2.settimeout(5.0)
+		#if server responds with success message, break from loop
+		if (str(s_3.recv(1024), "utf-8") == "success"):
+			print("Server confirmed matching character string")
 			
-			#remove comment to work with instructor-provided ROBOT executable
-			#s_2_port = int(s_1.recv(1024).decode("utf-8"))
+			#close third socket
+			s_3.close()
 			
-			s_2_port = int.from_bytes(s_1.recv(1024), byteorder="big")
-			s_2.bind(("127.0.0.1", s_2_port))
-			s_2.listen()
-			print("Listening on port {}".format(s_2_port))
-			connection, address = s_2.accept()
-			
-			#when server connects, extract ports
-			with connection:
-				
-				#close first socket
-				s_1.close()
-				#remove comment to work with instructor-provided ROBOT executable
-				#ports = connection.recv(1024).decode("utf-8").split(",")
-				#server_port = int(ports[0])
-				#client_port = int(ports[1])
-				
-				ports = str(connection.recv(1024), "utf-8").split(",")
-				server_port = int(ports[0])
-				client_port = int(ports[1].split(".")[0])
-				
-				#close second socket
-				s_2.close()
-				
-				#create third socket - UDP
-				s_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-				
-				#bind UDP socket to client port, set 5 second timeout
-				s_3.bind(("127.0.0.1", client_port))
-				s_3.settimeout(5.0)
-				
-				#connect to server via UDP
-				s_3.connect(("127.0.0.1", server_port))
-				print("Connected to server on port {}".format(server_port))
-				
-				#wait for 1 second, then send random int to server
-				time.sleep(1.0)
-				s_3.send(bytes(random.randint(6, 9).to_bytes(10, byteorder="big")))
-				print("Sent random integer to server on port {}".format(server_port))
-				
-				#send random characters received as a response back to server five times; once per second
-				for i in range(5):
-					time.sleep(1.0)
-					s_3.send(s_3.recv(1024))
-					print("Sending random string of characters back to server on {}".format(server_port))
-					
-					#if server responds with success message, break from loop
-					if (str(s_3.recv(1024), "utf-8") == "success"):
-						print("Server confirmed matching character string")
-						
-						#close third socket
-						s_3.close()
-						
-						break
+			break
